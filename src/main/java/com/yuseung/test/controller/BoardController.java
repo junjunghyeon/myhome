818b067b846144c7223d991e113bab9ce1bcdada
yuseung <bbohihi@gmail.com>
2021-06-20 17:39:08 +0900

    init5

--- src/main/java/com/yuseung/test/controller/BoardController.java
+++ src/main/java/com/yuseung/test/controller/BoardController.java
@@ -3,11 +3,12 @@
  */
 package com.yuseung.test.controller;
 
-import java.util.List;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -34,8 +35,14 @@ public class BoardController {
 	private BoardValidator boardValidator;
 	
 	@GetMapping("/list")
-	public String list(Model model) {
-		List<Board> boards = boardRepository.findAll();
+	public String list(Model model, @PageableDefault(size = 2) Pageable pageable, @RequestParam(required=false, defaultValue="") String searchText) {
+		//Page<Board> boards = boardRepository.findAll(pageable);
+		Page<Board> boards = boardRepository.findByTitleContainingOrContentContaining(searchText, searchText, pageable);
+		int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+		int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
+		
+		model.addAttribute("startPage", startPage);
+		model.addAttribute("endPage", endPage);
 		model.addAttribute("boards", boards);
 		
 		return "board/list";
--- src/main/java/com/yuseung/test/repository/BoardRepository.java
+++ src/main/java/com/yuseung/test/repository/BoardRepository.java
@@ -5,6 +5,8 @@ package com.yuseung.test.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.yuseung.test.model.Board;
@@ -16,4 +18,6 @@ import com.yuseung.test.model.Board;
 public interface BoardRepository extends JpaRepository<Board, Long> {
 	List<Board> findByTitle(String title);
 	List<Board> findByTitleOrContent(String title, String content);
+	
+	Page<Board> findByTitleContainingOrContentContaining(String title, String contecnt, Pageable pageable);
 }
